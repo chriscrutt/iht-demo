a1677074d72f2c69dec7624cfffd203a824af99f
Chris <chriscrutt@gmail.com>
2023-04-12 20:01:06 -0700

    init

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+"use strict";
+$(document).ready(function() {
+
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        AOS Animation Activation
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    AOS.init();
+    window.addEventListener("load", AOS.refresh);
+    AOS.init({
+        once: true
+    })
+
+    
+
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+           Sticky Header
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 50 ||
+            document.documentElement.scrollTop > 50
+        ) {
+            $(".site-header--sticky").addClass("scrolling");
+        } else {
+            $(".site-header--sticky").removeClass("scrolling");
+        }
+        if (
+            document.body.scrollTop > 700 ||
+            document.documentElement.scrollTop > 700
+        ) {
+            $(".site-header--sticky.scrolling").addClass("reveal-header");
+        } else {
+            $(".site-header--sticky.scrolling").removeClass("reveal-header");
+        }
+    }
+
+    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      
+          Preloader Activation
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+    $(window).load(function() {
+        setTimeout(function() {
+            $("#loading").fadeOut(500);
+        }, 1000);
+        setTimeout(function() {
+            $("#loading").remove();
+        }, 2000);
+    });
+
+
+
+
+    
+
+});
